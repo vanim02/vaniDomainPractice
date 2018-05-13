d7e4aad06ce750ac01d06567cce9dc49ac83492f
vani <vanim02@gmail.com>
2018-05-12 20:25:02 -0400

    startup updated

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 yum install git -y
-cd /home/ec2-user/vaniDomainPractice
-watch -n -x 10 git pull origin master
+sudo git clone https://github.com/vanim02/vaniDomainPractice.git
+
