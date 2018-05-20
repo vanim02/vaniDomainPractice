a8b5239c49f0106dc54ddcbbdcd53afd41d10dd5
Cloud User <ec2-user@ip-10-0-0-96.ec2.internal>
2018-05-20 23:29:15 +0000

    code improved

--- autoscaling.sh
+++ autoscaling.sh
@@ -2,54 +2,64 @@
 ## retrieve instance id
 instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 ## compare with pilot instance
-if [ $instanceId != i-0d371ecf0e1b145dd ] 
+if [ $instanceId != i-0d371ecf0e1b145dd ]
 then
-	exit 0
-else
-##git pull for every 30 mins 
-cd /home/ec2-user/vaniDomainPractice && sudo git pull origin master
-sleep 30s
+			exit 0
+	else
+	##git pull from origin
+	cd /home/ec2-user/vaniDomainPractice && sudo git pull origin master
+	sleep 30s
 
-cd /home/ec2-user/
+	cd /home/ec2-user/
 
-echo "create image for the pilot instance"
-aws ec2 create-image --instance-id i-0d371ecf0e1b145dd --name vani-img-$(date -u +\%Y\%m\%dT\%H\%M\%S) --region us-east-1 --no-reboot
+	echo "create image for the pilot instance"
+	latestami=$(aws ec2 create-image --instance-id i-0d371ecf0e1b145dd --name vani-img-$(date -u +\%Y\%m\%dT\%H\%M\%S) --region us-east-1 --output text --no-reboot)
 
-sleep 2m
+	echo "ami created is $latestami"
 
-##retrieve the latest image and assign it to create a launch-configuration
-latestami=$(aws ec2 describe-images --region us-east-1 --owners self --query 'Images[].[CreationDate,ImageId]' --output table | sort -k1r | grep ami |head -n1|awk '{print $4}') 
-
-aws autoscaling create-launch-configuration --launch-configuration-name vanilc$(date -u +\%Y\%m\%dT\%H\%M\%S)  --image-id $latestami --instance-type t2.micro --region us-east-1
-
-sleep 2m
-
-echo "launch config in process"
-
-###retrieve the latest launch-configurations and assign it to create autoscaling group
-latestLc=$(aws autoscaling  describe-launch-configurations --region us-east-1 --output table  --query 'LaunchConfigurations[].[CreatedTime,LaunchConfigurationName,InstanceType]' | grep t2 | sort -k1r | head -n1 | awk '{print $4}')
-echo "Latest LC is $latestLc"
-
-##retrieve the list of auto-scaling-group into the array
-asg=$(aws autoscaling describe-auto-scaling-groups  --query 'AutoScalingGroups[].[AutoScalingGroupName]' --output text| grep ^vaniasg$)
-	
-echo "ASG is $asg"
-
-if [ $asg == vaniasg ] 
-then
-echo "vani asg exists"
-	aws autoscaling update-auto-scaling-group --auto-scaling-group-name vaniasg --launch-configuration-name $latestLc --min-size 2 --max-size 4 --vpc-zone-identifier subnet-a5827ec2 --termination-policies "OldestLaunchConfiguration"
-
-sleep 90
-echo "scale up in process" 
-	aws autoscaling put-scheduled-update-group-action --scheduled-action-name ScaleUp --auto-scaling-group-name vaniasg --start-time "$(date -d "+30 secs")" --desired-capacity 4
-
-sleep 90
-echo "scale down in process"
+	imageState=$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --output text)
+	while [ $imageState != "available" ]
+	do
+	echo "image status now is $imageState"
+	   sleep 30
+	imageState="$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --output text)"
+	done
 		
-	 aws autoscaling put-scheduled-update-group-action --scheduled-action-name ScaleDown --auto-scaling-group-name vaniasg --start-time "$(date -d "+30 secs")"  --desired-capacity 2 
-else
-echo "vani asg does not exists"
-	aws autoscaling create-auto-scaling-group --auto-scaling-group-name vaniasg --launch-configuration-name $latestLc --min-size 2 --max-size 4 --load-balancer-names vanilb --vpc-zone-identifier subnet-a5827ec2 --region us-east-1 
+		lcName=$(echo vanilc$(date -u +\%Y\%m\%dT\%H\%M\%S))
+		echo "launch config in process"
+		aws autoscaling create-launch-configuration --launch-configuration-name $lcName  --image-id $latestami --instance-type t2.micro --region us-east-1
+	
+	
+	lcState=$(aws autoscaling describe-launch-configurations --launch-configuration-name $lcName | grep ImageId)
+	while [ -z "$lcState" ]
+	do
+	echo "image status now is $lcState"
+		sleep 30
+	done
+	
+		
+	if [ -z "$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name vaniasg --output text)" ]
+	then
+		 echo "vani asg does not exists"
+			aws autoscaling create-auto-scaling-group --auto-scaling-group-name vaniasg --launch-configuration-name $lcName --min-size 2 --max-size 4 --load-balancer-names vanilb --vpc-zone-identifier subnet-a5827ec2 --region us-east-1
+	else
+		  
+	echo "vani asg exists"
+			aws autoscaling update-auto-scaling-group --auto-scaling-group-name vaniasg --launch-configuration-name $lcName --min-size 0 --max-size 4 --vpc-zone-identifier subnet-a5827ec2 --termination-policies "OldestLaunchConfiguration"
+	echo "updating launch configuration"
+			
+			while [ "$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name vaniasg --query 'AutoScalingGroups[].[LaunchConfigurationName]' --output text)" != "$lcName" ]
+			do
+				echo "LC not updated yet"
+				sleep 30
+			done
+	sleep 10					 
+echo "scale up in process..."	 
+aws autoscaling set-desired-capacity --auto-scaling-group-name vaniasg --desired-capacity 2
+	sleep 10
+echo "scale down in process..."
+	  aws autoscaling set-desired-capacity --auto-scaling-group-name vaniasg --desired-capacity 0	
 fi
+		
 fi
+
