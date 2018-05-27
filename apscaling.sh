372a910efd963060c1450ed78095cd4e68efedd6
root <root@ip-10-0-1-241.ec2.internal>
2018-05-27 18:08:02 +0000

    Final SCRIPT COMMIT for DEMO

--- apscaling.sh
+++ apscaling.sh
@@ -2,7 +2,7 @@
 ## retrieve instance id
 instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 ## compare with pilot instance
-if [ $instanceId != i-05ea9bde81ddb9bcc ]
+if [ $instanceId != i-0b6b289505e143392 ]
 then
 			exit 0
 	else
@@ -13,24 +13,24 @@ then
 	cd /home/ec2-user/
 
 	echo "create image for the pilot instance"
-	latestami=$(aws ec2 create-image --instance-id i-05ea9bde81ddb9bcc --name ap-img-$(date -u +\%Y\%m\%dT\%H\%M\%S) --region us-east-1 --output text --no-reboot)
+	latestami=$(aws ec2 create-image --instance-id i-0b6b289505e143392 --name ap-img-$(date -u +\%Y\%m\%dT\%H\%M\%S) --region us-east-1 --output text --no-reboot)
 
 	echo "ami created is $latestami"
 
-	imageState=$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --output text)
+	imageState=$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --region us-east-1 --output text)
 	while [ $imageState != "available" ]
 	do
 	echo "image status now is $imageState"
 	   sleep 30
-	imageState="$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --output text)"
+	imageState="$(aws ec2 describe-images --image-id $latestami --query 'Images[].[State]' --owners self --region us-east-1 --output text)"
 	done
 		
 		lcName=$(echo adapalc$(date -u +\%Y\%m\%dT\%H\%M\%S))
 		echo "launch config in process"
-		aws autoscaling create-launch-configuration --launch-configuration-name $lcName  --image-id $latestami --instance-type t2.micro --region us-east-1
+		aws autoscaling create-launch-configuration --launch-configuration-name $lcName  --image-id $latestami --instance-type t2.small --region us-east-1
 	
 	
-	lcState=$(aws autoscaling describe-launch-configurations --launch-configuration-name $lcName | grep ImageId)
+	lcState=$(aws autoscaling describe-launch-configurations --launch-configuration-name $lcName --region us-east-1 | grep ImageId)
 	while [ -z "$lcState" ]
 	do
 	echo "launch configuration status now is $lcState"
@@ -38,14 +38,14 @@ then
 	done
 	
 		
-	if [ -z "$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name adapaasg --output text)" ]
+	if [ -z "$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name adapaasg --region us-east-1 --output text)" ]
 	then
-		 echo "adapaasg does not exists......creating"
-			aws autoscaling create-auto-scaling-group --auto-scaling-group-name adapaasg --launch-configuration-name $lcName --min-size 2 --max-size 4 --load-balancer-names adapalb --vpc-zone-identifier subnet-90c771da --vpc-zone-identifier subnet-85ff13ab --region us-east-1
+		 echo "adapaasg does not exists......creating a new asg"
+			aws autoscaling create-auto-scaling-group --auto-scaling-group-name adapaasg --launch-configuration-name $lcName --min-size 2 --max-size 4 --load-balancer-names DemoELB --vpc-zone-identifier subnet-90c771da --vpc-zone-identifier subnet-85ff13ab --region us-east-1
 	else
 		  
 	echo "adapa asg exists......updating"
-			aws autoscaling update-auto-scaling-group --auto-scaling-group-name adapaasg --launch-configuration-name $lcName --min-size 0 --max-size 4 --vpc-zone-identifier subnet-85ff13ab --vpc-zone-identifier subnet-90c771da --termination-policies "OldestLaunchConfiguration"
+			aws autoscaling update-auto-scaling-group --auto-scaling-group-name adapaasg --launch-configuration-name $lcName --min-size 0 --max-size 4 --vpc-zone-identifier subnet-85ff13ab --vpc-zone-identifier subnet-90c771da --region us-east-1 --termination-policies "OldestLaunchConfiguration"
 	echo "updating launch configuration"
 			
 			while [ "$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name adapaasg --query 'AutoScalingGroups[].[LaunchConfigurationName]' --output text)" != "$lcName" ]
@@ -56,9 +56,9 @@ then
 	sleep 10					 
 echo "scale up in process..."	 
 aws autoscaling set-desired-capacity --auto-scaling-group-name adapaasg --desired-capacity 2
-	sleep 10
+	sleep 300
 echo "scale down in process..."
-	  aws autoscaling set-desired-capacity --auto-scaling-group-name adapaasg --desired-capacity 0	
+	  aws autoscaling set-desired-capacity --auto-scaling-group-name adapaasg --desired-capacity 3	
 fi
 		
 fi
